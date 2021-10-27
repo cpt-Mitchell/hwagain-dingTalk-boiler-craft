a71f3d612dea4fe78489cb523c03d15c003e6ac8
cpt-Mitchell <695046612@qq.com>
2021-10-27 18:11:43 +0800

    test

--- src/utils/httpUtil.js
+++ src/utils/httpUtil.js
@@ -9,10 +9,19 @@ const fetch = axios.create({
   timeOut: 5000
 })
 
+const userStorage = function() {
+  return DingTalkApi.util.domainStorage.getItem({ name: 'loginuser' }).then(res => {
+    dAlert(JSON.stringify(res))
+    window.localStorage.setItem('ding_user_id', res.userid || '')
+    return res.userid || ''
+  })
+}
+
 fetch.interceptors.request.use(
   config => {
     if (config.url !== API.DINGTALK_USERID) {
       config.headers.common['Authorization'] = vuet.modules.home._LOGINUSER_.token
+      config.headers.common['ding_user_id'] = userStorage() /* 'A011814' */
     }
     return config
   },
@@ -47,7 +56,7 @@ fetch.interceptors.response.use(
           break
       }
       if (error.response.status === 401) {
-        window.location.reload()
+        // window.location.reload()
       } else if (errMsg) {
         dd.device.notification.alert({
           message: errMsg || error.response.msg || '',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,340 +1,154 @@
 <template>
-  <section class="container">
-    <!-- v-if="user.name" -->
-    <div class="search-box">
-      <div class="row-box">
-        <div class="form-item">
-          <label class="label-text date-s-label">日期</label>
-          <div style="display: flex;align-items: center;">
-            <div @click="dateSelector('startDate')">
-              <input disabled type="text" placeholder="请选择开始日期" class="input" v-model="startDate" />
-            </div>
-          </div>
-          <div class="form-item">
-            <span class="center-txt date-e-lable">至</span>
-            <div @click="dateSelector('endDate')">
-              <input disabled type="text" placeholder="请选择结束日期" class="input" v-model="endDate" />
-            </div>
-          </div>
-          <div class="form-item">
-            <label class="label-text state-lable">状态</label>
-            <div style="display: flex;align-items: center;" @click="stateSelector">
-              <input disabled type="text" placeholder="请选择状态" class="input state-input" v-model="currState" />
-            </div>
-          </div>
-        </div>
+  <div>
+    <div style="position: fixed; width: 100vw; z-index: 1; font-size: 16px">
+      <div style="height: 40px; display: flex; align-items: center;padding: 10px">
+        <span>检验人：{{ checker }}</span>
       </div>
-      <div class="row-box">
-        <div class="form-item">
-          <label class="label-text keywords-label">状态</label>
-          <div style="display: flex;align-items: center;">
-            <input type="text" placeholder="关键字搜索" class="input keywords-input" v-model="keywords" />
-          </div>
-        </div>
-        <button class="search-btn" @click="doSearch">查询</button>
+      <div style="height: 40px; display: flex; align-items: center;padding: 10px">
+        <span>取样时间：{{ checkTime }}</span>
       </div>
     </div>
-    <div class="approval-items">
-      <scroller ref="listRef" :on-refresh="refresh" :on-infinite="infinite">
-        <div class="approval-item-box" v-for="(item, index) in list" :key="item.AppID">
-          <div
-            class="approval-type"
-            :class="{ gw: item.TEM_Name.indexOf('公文') !== -1, qt: item.TEM_Name.indexOf('公文') === -1 }"
-          >
-            {{ item.TEM_Name.indexOf('公文') !== -1 ? '公文' : '其他' }}
-          </div>
-          <div class="approval-item">
-            <div class="title dwwo-2">{{ index + 1 }}. {{ item.AppO_Title }}</div>
-            <div class="sub-title">
-              <div class="applier">发起人：{{ getApplyName(item) }}</div>
-              <div class="task-name dwote">节点：{{ item.AppD_Name }}</div>
-              <div class="handle">当前处理人：{{ item.regNames }}</div>
-            </div>
-          </div>
-        </div>
-      </scroller>
+    <div class="cantain" style="padding-top: 80px">
+      <table class="todo-table">
+        <thead style="position: fixed; width: 100vw; background-color: rgb(52, 196, 253); z-index: 0">
+          <th colspan="2" style="width: 40vw; padding: 10px; font-size: 16px">蒸发站</th>
+          <th style="width: 20vw; padding: 10px; font-size: 16px">温度</th>
+          <th style="width: 20vw; padding: 10px; font-size: 16px">波美度</th>
+          <th style="width: 20vw; padding: 10px; font-size: 16px">备注</th>
+        </thead>
+      </table>
     </div>
-  </section>
+    <div class="cantain" style="padding-top: 38px; padding-bottom: 45px">
+      <table class="todo-table">
+        <tbody>
+          <template v-for="(item, index) in todoList">
+            <tr :key="index">
+              <td :colspan="item.nn === '结晶效' ? 2 : 1" style="width: 20vw; padding: 15px 5px;font-size: 14px">
+                {{ item.nn || '-' }}
+              </td>
+              <td v-if="item.nn !== '结晶效'" style="width: 20vw; padding: 15px 5px;font-size: 14px">
+                {{ item.zn || '-' }}
+              </td>
+              <td style="width: 20vw; padding: 15px 5px;font-size: 14px">{{ item.cn || '-' }}</td>
+              <td style="width: 20vw; padding: 15px 5px;font-size: 14px">{{ item.in || '-' }}</td>
+              <td style="width: 20vw; padding: 15px 5px;font-size: 14px">{{ item.dn || '-' }}</td>
+            </tr>
+          </template>
+        </tbody>
+      </table>
+    </div>
+    <div style="position: fixed; bottom: 0; height: 45px; border-top: 1px solid #ccc; z-index: 1">
+      <div style="display: flex; justify-content: center; item-align: center; padding: 5px 0; width: 100vw">
+        <button class="search-btn" @click="confirm">提交</button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import { API } from '@/api'
-import request from '@/utils/httpUtil'
-import { getLastDayOfMonthStr, getFirstDayOfMonthStr } from '@/utils/DateTimeUtil'
-import { mapModules } from 'vuet'
-
 export default {
-  mixins: [mapModules({ home: 'home' })],
   data() {
     return {
-      isLoading: false,
-      currPage: 0,
-      pageSize: 20,
-      stateOptions: ['全部', '办理中', '已结束', '暂停'],
-      list: [],
-      currState: '办理中',
-      keywords: '',
-      startDate: '2020-10-01' || getFirstDayOfMonthStr(),
-      endDate: getLastDayOfMonthStr()
+      checker: '曾令霜',
+      checkTime: '2021-10-27',
+      userName: '',
+      todoList: [
+        { nn: '结晶效', zn: 1, cn: 2, in: 3, dn: 4 },
+        { nn: '1#蒸发站', zn: 1, cn: 2, in: 3, dn: 4 }
+      ]
     }
   },
   mounted() {
     dd.biz.navigation.setTitle({
-      title: 'OA公文审批进度查询', // 控制标题文本，空字符串表示显示默认文本
+      title: '蒸发黑液浓度人工检测结果录入', // 控制标题文本，空字符串表示显示默认文本
       onSuccess: function(result) {},
       onFail: function(err) {}
     })
+    this.getUserInfo()
   },
   methods: {
-    getApplyName(item) {
-      return [
-        'gzszfkyzrhsd_a8290550-f2ba-42e4-bc21-10a8d77c4561',
-        'wgfkyzrhsd_f81a50c5-6c98-4f65-b35d-22ff248bc69b'
-      ].includes(item.Form_ID)
-        ? '保密'
-        : item.UserName
-    },
-    dateSelector(key) {
-      dd.biz.util.datepicker({
-        format: 'yyyy-MM-dd', // 注意：format只支持android系统规范，即2015-03-31格式为yyyy-MM-dd
-        value: this[key], // 默认显示日期
-        onSuccess: result => {
-          if (result.value) {
-            if (
-              key === 'startDate' &&
-              this.endDate &&
-              new Date(result.value).getTime() > new Date(this.endDate).getTime()
-            ) {
-              dAlert('开始日期不能大于结束日期')
-            } else if (
-              key === 'endDate' &&
-              this.startDate &&
-              new Date(this.startDate).getTime() > new Date(result.value).getTime()
-            ) {
-              dAlert('结束日期不能小于开始日期')
-            } else {
-              this[key] = result.value
-            }
-          }
-        },
-        onFail: function(err) {}
-      })
-    },
-    stateSelector() {
-      dd.device.notification.actionSheet({
-        title: '请选择流程状态', // 标题
-        cancelButton: '取消', // 取消按钮文本
-        otherButtons: this.stateOptions,
-        onSuccess: result => {
-          if (result.buttonIndex !== -1) {
-            this.currState = this.stateOptions[result.buttonIndex]
-          }
-          // onSuccess将在点击button之后回调
-          /* {
-            buttonIndex: 0 //被点击按钮的索引值，Number，从0开始, 取消按钮为-1
-        } */
-        },
-        onFail: function(err) {}
-      })
-    },
-    doSearch() {
-      this.$refs.listRef.triggerPullToRefresh()
-    },
-    refresh(done) {
-      this.getApprovalList(
-        1,
-        data => {
-          this.currPage = 1
-          this.list = data
-          done && done()
-        },
-        done
-      )
-    },
-    infinite(done) {
-      this.getApprovalList(
-        this.currPage + 1,
-        data => {
-          if (data.length === 0) {
-            done(true)
-          } else {
-            this.currPage += 1
-            this.list = this.list.concat(data)
-            done()
-          }
-        },
-        done
-      )
-    },
-    getApprovalList(pageNo, successcb, errorCb) {
-      let params = {
-        formid: '_',
-        apvResult: '同意',
-        module: 'OA查看审核审批进度',
-        task: '查询经办审核审批进度',
-        payload: {
-          strWFStatus: this.stateOptions.indexOf(this.currState) - 1,
-          strTitle: this.keywords || '',
-          strBginTimeL: this.startDate,
-          strBginTimeR: this.endDate,
-          intPageNo: pageNo,
-          intPageSize: 20,
-          strFlowType: '',
-          strPrivacy: '',
-          strGwspStatus: '',
-          strIsSec: ''
-        }
+    getUserInfo() {
+      this.userName = window.localStorage.getItem('ding_user_id') || 'A011814'
+      if (this.userId) {
+        this.userCode = Encrypt(this.userId)
+        this.doSearch()
       }
-      request
-        .post(API.DINGTALK_GET_APPROVAL_PROGRESS, params)
-        .then(res => {
-          console.log(res)
-          let data = res.data || {}
-          if (data.errorCode === 0) {
-            // this.list = data.data || []
-            console.log(data.data.data)
-            successcb && successcb(data.data.data || [])
-          } else {
-            errorCb && errorCb()
-            dAlert(data.msg || '查询失败')
-          }
-        })
-        .catch(err => {
-          errorCb && errorCb()
-          console.error(`请求${API.DINGTALK_MICROAPP_USER}失败`, err)
-        })
-    }
+    },
+    confirm() {}
   }
 }
 </script>
 
-<style lang="less">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  overflow: hidden;
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue',
-    Arial, sans-serif;
-  // display: block;
-  color: #35495e;
-  letter-spacing: 1px;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-  // text-align: center;
-  .search-box {
-    padding: 20px 10px;
-    background: #fff;
-    .row-box {
-      box-sizing: border-box;
-      display: flex;
-      align-items: center;
-      flex-direction: row;
-      &:not(:last-child) {
-        margin-bottom: 15px;
-      }
-      .form-item {
-        display: flex;
-        align-items: center;
-      }
-    }
-    .date-s-label {
-      width: 80px;
-    }
-    .input {
-      width: 190px;
-      font-size: 26px;
-      height: 66px;
-    }
-    .state-input {
-      width: 120px;
-    }
-    .keywords-label {
-      width: 80px;
-    }
-    .keywords-input {
-      width: 435px;
-    }
-    .search-btn {
-      padding: 16px 60px;
-      border: none;
-      background: #0099ff;
-      color: #fff;
-      margin-left: 25px;
-      &:active {
-        opacity: 0.8;
-      }
-    }
-    .label-text {
-      padding: 5px 10px;
-      // margin-right: 5px;
-      display: inline-block;
-      text-align: center;
-    }
-    .center-txt {
-      padding: 0 15px;
-      display: inline-block;
+<style lang="less" scoped>
+/* .cantain {
+  padding-top: 72px;
+} */
+.search-btn {
+  padding: 16px 60px;
+  border: none;
+  background: #0099ff;
+  color: #fff;
+  margin-left: 25px;
+  &:active {
+    opacity: 0.8;
+  }
+}
+.van-cell {
+  background-color: #f0f2f5;
+  padding: 10px 15px;
+  height: 34px;
+  line-height: 18px;
+}
+.tips {
+  font-size: 14px;
+  min-height: 34px;
+  border-spacing: 0;
+  text-align: center;
+  border: 1px solid rgb(52, 196, 253);
+  border-left: none;
+  border-right: none;
+  border-top: none;
+}
+.head {
+  font-size: 14px;
+  min-height: 36px;
+  border-spacing: 0;
+  font-weight: bold;
+  padding: 7px 0;
+  text-align: center;
+  border: 1px solid #ccc;
+  border-left: none;
+  border-top: none;
+}
+.van-field {
+  background-color: #f0f2f5;
+}
+.todo-table {
+  width: 100vw;
+  border-collapse: collapse;
+  border-spacing: 0;
+  th,
+  td {
+    text-align: center;
+    font-size: 14px;
+    height: 36px;
+  }
+  td,
+  th {
+    border: 1px solid #ccc;
+  }
+  td {
+    &:first-child {
+      border-left: none;
     }
   }
-  .approval-items {
-    margin-top: 20px;
-    height: calc(100vh - 160px - 20px);
-    background: #fff;
-    position: relative;
-    .approval-item-box {
-      display: flex;
-      flex-direction: row;
-      align-items: center;
-      border-bottom: 1px solid #e6e6e6;
-      .approval-type {
-        width: 60px;
-        height: 60px;
-        font-size: 20px;
-        color: #fff;
-        align-items: center;
-        display: flex;
-        justify-content: center;
-        border-radius: 50%;
-        margin-left: 10px;
-        &.gw {
-          background: #0099ff;
-        }
-        &.qt {
-          background: #00cc88;
-        }
-      }
+  td,
+  th {
+    &:first-child {
+      border-left: none;
     }
-    .approval-item {
-      display: block;
-      padding: 20px 20px 10px;
-      flex-grow: 1;
-      width: 0;
-      .title {
-        font-size: 28px;
-        color: #333;
-        margin-bottom: 10px;
-      }
-      .sub-title {
-        display: flex;
-        justify-items: center;
-        justify-content: space-between;
-        & > div {
-          font-size: 24px;
-          color: #666;
-        }
-        .applier {
-          width: 200px;
-        }
-        .handle {
-          width: 240px;
-          text-align: right;
-        }
-        .task-name {
-          text-align: center;
-          width: 220px;
-          padding: 0 5px;
-        }
-      }
+    &:last-child {
+      border-right: none;
     }
   }
 }
